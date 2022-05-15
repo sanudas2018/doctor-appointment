d68d89ad5e85543aeddd1b638eaec2140ca6c648
sanudas2018 <programing26@gmail.com>
2022-05-15 12:19:02 +0600

    Appion- backgroun set

--- src/Components/Pages/Appiontment/AppionBanner/AppionBanner.js
+++ src/Components/Pages/Appiontment/AppionBanner/AppionBanner.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import bangarImag from '../../../../assets/images/chair.png'
-
+import bgimg from '../../../../assets/images/bg.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { format } from 'date-fns';
@@ -8,9 +8,13 @@ import { format } from 'date-fns';
 const AppionBanner = () => {
    const [date, setDate] = useState(new Date());
    return (
-         <div className='hero min-h-screen '>
-            <div className='hero-content flex-col lg:flex-row-reverse '>
-               <img className='w-6/12 rounded-lg shadow-xl' src={bangarImag} alt=''/>
+         <div
+         style={{
+            background:`url(${bgimg})`
+         }}
+         className='hero min-h-screen'>
+            <div className='hero-content flex flex-col justify-between lg:flex-row-reverse '>
+               <img className='w-7/12 rounded-lg shadow-xl' src={bangarImag} alt=''/>
                <div className=''>
                <DayPicker
                   mode="single"
